4050b676549029adac69ef9e7d3e42fa00354fbf
Natalya Kozarezenko <wind_nv@mail.ru>
2025-01-21 12:15:43 +0300

    test sign in admin and PEP8

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS=['https://allfood.zapto.org/']
+CSRF_TRUSTED_ORIGINS = ['https://allfood.zapto.org/']
